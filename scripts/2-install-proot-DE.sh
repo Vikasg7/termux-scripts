389878cd87eb20858067dec56f27dfde89a2512c
Vikas Gautam <v33vikasgautam@gmail.com>
2024-02-15 02:38:08 +0530

    wip: adds lxmusic player to the apt deps

--- scripts/2-install-proot-DE.sh
+++ scripts/2-install-proot-DE.sh
@@ -44,7 +44,7 @@ $USERNAME	ALL=(ALL:ALL) ALL
 " >> /etc/sudoers
 
 # install DE 
-apt install -y xfce4 xfce4-whiskermenu-plugin xfonts-base xfce4-terminal dbus-x11 tigervnc-standalone-server tigervnc-tools
+apt install -y xfce4 xfce4-whiskermenu-plugin lxmusic xfonts-base xfce4-terminal dbus-x11 tigervnc-standalone-server tigervnc-tools
 
 # switch to user
 su - vikas -c "
