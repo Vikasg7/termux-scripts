b52ce3e92b193d0709fd41efda07bc46ea97d240
Vikas Gautam <v33vikasgautam@gmail.com>
2024-02-15 17:38:57 +0530

    fix: adds shebang in start-proot-distro

--- scripts/3-start-proot-distro.sh
+++ scripts/3-start-proot-distro.sh
@@ -1,1 +1,2 @@
+#!/data/data/com.termux/files/usr/bin/sh
 proot-distro login debian --user vikas --no-kill-on-exit --shared-tmp --bind $HOME/storage:/mnt/Phone -- /bin/bash -c 'rm -rf /tmp/.X* && vncserver -autokill yes'
