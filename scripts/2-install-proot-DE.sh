be91fe9d2a3ea9a9eb0428c33d2fda7fe4a1c774
Vikas Gautam <v33vikasgautam@gmail.com>
2024-02-21 02:08:08 +0530

    adds for apt deps

--- scripts/2-install-proot-DE.sh
+++ scripts/2-install-proot-DE.sh
@@ -44,9 +44,10 @@ $USERNAME	ALL=(ALL:ALL) ALL
 " >> /etc/sudoers
 
 # install DE f
-apt install -y xfce4 xfce4-whiskermenu-plugin audacious xfonts-base xfce4-terminal dbus-x11 file-roller chromium wget\
-               tigervnc-standalone-server tigervnc-tools at-spi2-core ristretto mousepad aptitude git make tumbler\
-               thunar-archive-plugin xfce4-notifyd xfce4-screenshooter xfce4-taskmanager parole policykit-1-gnome 
+apt install -y xfce4 xfce4-whiskermenu-plugin audacious xfonts-base xfce4-terminal dbus-x11 file-roller chromium wget \
+               tigervnc-standalone-server tigervnc-tools at-spi2-core ristretto mousepad aptitude git make tumbler \
+               thunar-archive-plugin xfce4-notifyd xfce4-screenshooter xfce4-taskmanager parole policykit-1-gnome nano \
+               bash-completion librsvg2-common
 
 source <(curl -sSL https://raw.githubusercontent.com/Vikasg7/termux-scripts/main/scripts/3-install-mint-themes.sh)
 
